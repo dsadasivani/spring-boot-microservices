eab8ccf8f6f91092ab588f9739a1579742e278c2
Dilip Kumar Patnaik Sadasivani <dsadasivani@gmail.com>
2024-04-22 18:31:48 +0530

    inventory service code

---
+++ inventory-service/src/main/java/com/dilip/microservices/inventory/repository/InventoryRepository.java
@@ -0,0 +1,8 @@
+package com.dilip.microservices.inventory.repository;
+
+import com.dilip.microservices.inventory.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, int quantity);
+}
---
+++ inventory-service/src/main/resources/db/migration/V2__add_inventory.sql
@@ -0,0 +1,3 @@
+insert into t_inventory (sku_code, quantity) values ('iphone_13', 100);
+insert into t_inventory (sku_code, quantity) values ('iphone_13_pro', 100);
+insert into t_inventory (sku_code, quantity) values ('iphone_13_pro_max', 100);
